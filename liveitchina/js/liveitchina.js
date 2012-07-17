f081b9ab75db367caf6c656c64367493ecc4a356
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-07-17 16:42:22 +0800

    clean up the javascript a little bit including the promote school on search school page

--- liveitchina/js/liveitchina.js
+++ liveitchina/js/liveitchina.js
@@ -1,30 +1,45 @@
 (function ($) {
-  $(document).ready(function(){
-    var isSchool = Drupal.settings.lic_search.school;
+  Drupal.behaviors.liveitchina = {
+    attach: function(context) {
+      var isProfileBack = Drupal.settings.lic.profile_back;
+      var isSchool = Drupal.settings.lic.school;
 
-    $(".show-menu").click(function(e){
-        e.preventDefault();
-        if($(".show-menu").hasClass("menu-open")){
-             $(".show-menu").removeClass("menu-open");
-             $("ul#display-menu.children").hide();
-         
-        } else {
-           $("ul#display-menu.children").show();
-           $(".show-menu").addClass("menu-open");
-        }
-    });
-    
-    $("ul#display-menu.children").mouseup(function() {return false});
-    $(".show-menu").mouseup(function() {return false});
-    $(document).mouseup(function(e){
+      if (isProfileBack) {
+        $('#region-content h1#page-title', context).append('<a class="back" href="#"> << ' + Drupal.t('Back to Search Results') + '</a>');
+        $('a.back').click(function(){
+          parent.history.back();
+          return false;
+        });
+      }
+
+      if (isSchool){
+        $('#region-content h1#page-title').append('<a class="back" href="'+ Drupal.settings.lic.url +'"> ' + Drupal.t('Promote my School') + '</a>');
+      }
+
+      $(".show-menu").click(function(e){
+          e.preventDefault();
+          if($(".show-menu").hasClass("menu-open")){
+               $(".show-menu").removeClass("menu-open");
+               $("ul#display-menu.children").hide();
+           
+          } else {
+             $("ul#display-menu.children").show();
+             $(".show-menu").addClass("menu-open");
+          }
+      });
+
+      $("ul#display-menu.children").mouseup(function() {
+        return false
+      });
+      $(".show-menu").mouseup(function() {
+        return false
+      });
+      $(document).mouseup(function(e){
         if($(e.target).parent("a.show-menu").length==0){
-            $(".show-menu").removeClass("menu-open");
-            $("ul#display-menu.children").hide();
+          $(".show-menu").removeClass("menu-open");
+          $("ul#display-menu.children").hide();
         }
-    });
-    
-    if (isSchool){
-      $('#region-content h1#page-title').append('<a class="back" href="'+ Drupal.settings.lic_search.url +'"> ' + Drupal.t('Promote my School') + '</a>');
+      });
     }
-  });
+  };
 })(jQuery);
