2831611462215175078b669d9c0b2337edd3f7c8
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-07-17 15:27:51 +0800

    drupal.t

--- liveitchina/modules/profile2_liveitchina/js/lic_profile.js
+++ liveitchina/modules/profile2_liveitchina/js/lic_profile.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.lic_profile = {
     attach: function(context) {
-      $('#region-content h1#page-title', context).append('<a class="back" href="#"> Back</a>');
+      $('#region-content h1#page-title', context).append('<a class="back" href="#"> << ' + Drupal.t('Back to Search Results') + '</a>');
       $('a.back').click(function(){
           parent.history.back();
           return false;
