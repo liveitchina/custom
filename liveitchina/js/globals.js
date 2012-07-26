26850a26def44584212300ce8b686f2e9466bbed
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-07-26 11:26:04 +0800

    change the style for the locale menu switcher

--- liveitchina/js/globals.js
+++ liveitchina/js/globals.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function () {
+  /*
   jQuery('#block-locale-language .block-content ul').each(function(){
     var list=jQuery(this),
     sortform = jQuery(document.createElement('form')).insertBefore(jQuery(this).hide()).attr('id','form-switcher-locale');
@@ -6,40 +7,41 @@ jQuery(document).ready(function () {
     select.change(function(){
       window.location = jQuery(this).val();
     });
-        jQuery('>li a', this).each(function(){
-          var target=jQuery(this).attr('target'),
+
+    jQuery('>li a', this).each(function(){
+      var target=jQuery(this).attr('target'),
           option=jQuery(document.createElement('option'))
-            .appendTo(select)
-            .val(this.href)
-            .html(jQuery(this).html());
-        });
-        list.remove();
-      });
-      
-      jQuery('.block-slider').each(function(index, value){
-        var link = jQuery(document.createElement('a')).attr({
+          .appendTo(select)
+          .val(this.href)
+          .html(jQuery(this).html());
+    });
+    list.remove();
+  });
+*/
+  jQuery('.block-slider').each(function(index, value){
+    var link = jQuery(document.createElement('a')).attr({
           "href":"#",
           "class":"slider-button"
         });
 
-        link.text("+").bind('click',function(index, value){
-          event.preventDefault();
-          jQuery(this).siblings('.block-content, .view').slideToggle('slow', function(){
-            var parent = jQuery(this).parent('.block-slider');
-            if (parent.hasClass('toggle-open')){
-              parent.removeClass('toggle-open').addClass('toggle-close');
-            }
-            else{
-              parent.removeClass('toggle-close').addClass('toggle-open');
-            }
-          });
-        });
+    link.text("+").bind('click',function(index, value){
+      event.preventDefault();
+      jQuery(this).siblings('.block-content, .view').slideToggle('slow', function(){
+      var parent = jQuery(this).parent('.block-slider');
+      if (parent.hasClass('toggle-open')){
+        parent.removeClass('toggle-open').addClass('toggle-close');
+      }
+      else{
+        parent.removeClass('toggle-close').addClass('toggle-open');
+      }
+    });
+  });
 
-        jQuery(this).append(link).addClass('toggle-open');
-        if (!jQuery(this).hasClass('connections')){
-          jQuery('.slider-button',this).trigger('click');
-        }
-      });
+  jQuery(this).append(link).addClass('toggle-open');
+    if (!jQuery(this).hasClass('connections')){
+      jQuery('.slider-button',this).trigger('click');
+    }
+  });
       
   jQuery('.hscroller-link').click(function(event){
     event.preventDefault();
