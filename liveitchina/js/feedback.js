8710741545859ecccc3733ba6546fc32ea97d014
Geoffrey Macasaet <geoffrey@iwoork.com>
2012-08-07 14:01:32 +0800

    Upload feedback jscript.

---
+++ liveitchina/js/feedback.js
@@ -0,0 +1,12 @@
+(function ($) {
+  Drupal.behaviors.liveitchina = {
+    attach: function(context) {
+  var uvOptions = {};
+  //(function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/Kqe5f3u6uUyZoGp8Js1WQ.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  //})();
+    }
+  };
+})(jQuery);
