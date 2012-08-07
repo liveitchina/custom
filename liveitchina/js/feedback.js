31e29dc44ac3509b911b0a80d31c3195c8ef8f03
Geoffrey Macasaet <geoffrey@iwoork.com>
2012-08-07 14:47:28 +0800

    change uservoice

--- liveitchina/js/feedback.js
+++ liveitchina/js/feedback.js
@@ -4,8 +4,8 @@
   var uvOptions = {};
   //(function() {
     var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-//    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/Kqe5f3u6uUyZoGp8Js1WQ.js';
-    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/037doNR5GhOAsK23dyMJQ.js';
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/Kqe5f3u6uUyZoGp8Js1WQ.js';
+    //uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/037doNR5GhOAsK23dyMJQ.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
   //})();
     }
