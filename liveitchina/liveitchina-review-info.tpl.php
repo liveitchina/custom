f76eaf62c7ed52fb360b1fd1459ad4a77018eb8b
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-08-10 12:26:55 +0800

    change grid class

--- liveitchina/liveitchina-review-info.tpl.php
+++ liveitchina/liveitchina-review-info.tpl.php
@@ -2,7 +2,7 @@
   <div class="user-info-image grid-2 alpha">
    <?php print l($image, $path, array('html'=>TRUE)); ?>
   </div>
-  <div class="user-info-detail grid-3 alpha">
+  <div class="user-info-detail grid-4 alpha">
     <h2><?php print $name; ?></h2>
     <span>(<?php print $profile['name'] ?>)</span>
   </div>
