c7e7e72ae2e0197ff45de20627501e29bc571c58
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-08-10 16:02:07 +0800

    add school image on reviews

--- liveitchina/liveitchina-review-info.tpl.php
+++ liveitchina/liveitchina-review-info.tpl.php
@@ -1,9 +1,10 @@
 <div class="user-info clearfix">
   <div class="user-info-image grid-2 alpha">
    <?php print l($image, $path, array('html'=>TRUE)); ?>
+   <?php print_r($entity); ?>
   </div>
   <div class="user-info-detail grid-4 alpha">
     <h2><?php print $name; ?></h2>
-    <span>(<?php print $profile['name'] ?>)</span>
+    <span>(<?php print $profile; ?>)</span>
   </div>
 </div>
