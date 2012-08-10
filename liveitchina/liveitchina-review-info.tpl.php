dee6165d6c37cf27ee29739731f511efc6e3b950
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-08-10 12:13:50 +0800

    add grid class

--- liveitchina/liveitchina-review-info.tpl.php
+++ liveitchina/liveitchina-review-info.tpl.php
@@ -1,8 +1,8 @@
 <div class="user-info clearfix">
-  <div class="user-info-image">
+  <div class="user-info-image grid-2 alpha">
    <?php print l($image, $path, array('html'=>TRUE)); ?>
   </div>
-  <div class="user-info-detail">
+  <div class="user-info-detail grid-3 alpha">
     <h2><?php print $name; ?></h2>
     <span>(<?php print $profile['name'] ?>)</span>
   </div>
