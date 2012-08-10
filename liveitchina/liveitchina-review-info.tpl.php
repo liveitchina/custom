40a9e7c5e73591f9fd6d02be4c9943b14df3709d
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-08-10 17:00:39 +0800

    add links

--- liveitchina/liveitchina-review-info.tpl.php
+++ liveitchina/liveitchina-review-info.tpl.php
@@ -1,10 +1,9 @@
 <div class="user-info clearfix">
   <div class="user-info-image grid-2 alpha">
    <?php print l($image, $path, array('html'=>TRUE)); ?>
-   <?php print_r($entity); ?>
   </div>
   <div class="user-info-detail grid-4 alpha">
-    <h2><?php print $name; ?></h2>
+    <h2><?php print l($name, $path); ?></h2>
     <span>(<?php print $profile; ?>)</span>
   </div>
 </div>
