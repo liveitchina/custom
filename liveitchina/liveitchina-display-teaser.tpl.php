9bb1562a67224bb7242e3a59577516db2ac4be6c
Jayson Nicolas <jayson@ilovehongkong.hk>
2012-08-16 23:43:41 +0800

    css

---
+++ liveitchina/liveitchina-display-teaser.tpl.php
@@ -0,0 +1,11 @@
+<div class="field-label-inline field field-name-field-lang-offered clearfix">
+  <div class="label-inline field-label label-inline"> <?php print t('Teaches')?>:&nbsp;</div>
+  <div class="field-items"><?php print $lang_display . $others_display ?></div>
+</div>
+<div class="field field-name-field-price field-type-list-text field-label-inline clearfix">
+  <div class="field-label"><?php print t('Cost')?>:&nbsp;</div>
+  <div class="field-items">
+    <div class="field-item even"><?php print $cost_currency ?></div>
+    <div class="field-item even field-name-field-discount"><?php print $discount ?></div>
+  </div>
+</div>
